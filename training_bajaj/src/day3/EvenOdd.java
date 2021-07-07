5d1a082d7ee213f894bc1f4edb3164f4447354cd
ashishwankhade <ashishwankhade@L-5CD11832P6.bfl.com>
2021-07-07 11:47:17 +0530

    commit

---
+++ training_bajaj/src/day3/EvenOdd.java
@@ -0,0 +1,31 @@
+package day3;
+import java.util.Scanner;  
+public class EvenOdd  
+{  
+	public static void main (String args[])  
+	{  
+		Student ashish = new Student();
+		
+		ashish.name = "Ashish";
+		ashish.age = 123;
+		//creating Scanner class object     
+		Scanner scan = new Scanner(System.in);  
+		System.out.print("Enter the number: ");  
+		//reading value from user  
+		int num=scan.nextInt();  
+		//method calling  
+		for(int i = 0; i < 3; i++) {
+			num = num + 5;
+		}
+		findEvenOdd(num);  
+	}  
+	//user defined method  
+	public static void findEvenOdd(int num)  
+	{  
+		//method body  
+		if(num%2==0)   
+			System.out.println(num+" is even");   
+		else   
+			System.out.println(num+" is odd");  
+	}  
+}  
---
+++ training_bajaj/src/day3/Student.java
@@ -0,0 +1,10 @@
+package day3;
+
+public class Student {
+	String name;
+	int age;
+	boolean isEligible;
+	
+	public Student() {} //empty constructor
+
+}
